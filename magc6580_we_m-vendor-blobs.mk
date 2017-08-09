a679f74184a64ce6314d02edfd4d6b3275bae81d
FeyoMx <elfeyo1980@gmail.com>
2017-08-08 23:19:23 -0500

    upload files

---
+++ magc6580_we_m-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bluboo/magc6580_we_m/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bluboo/magc6580_we_m/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bluboo/magc6580_we_m/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bluboo/magc6580_we_m/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bluboo/magc6580_we_m/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bluboo/magc6580_we_m/vendor,system/vendor)
